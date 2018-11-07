4cd3e99dfe12e469c3b2c8706a0e4ee8390bcb91
stalker474 <anton.possylkine@gmail.com>
2018-11-07 22:33:03 +0100

    draft 2 of horse auction contract (small correction)

--- contracts/HorseAuction.sol
+++ contracts/HorseAuction.sol
@@ -1,9 +1,7 @@
 pragma solidity ^0.4.24;
 
-import "./WalletUser.sol";
 import "../openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 import "../openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "../openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "../openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 /**
